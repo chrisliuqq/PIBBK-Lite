8f53bf876cc8a148b95d031a3df6b1b17e3a9511
Chris Liu <chrisliuqq@gmail.com>
2020-06-16 17:25:00 +0800

    [fix] 修正小說家抓取標題時支援章節與話數 by Player

--- src/parser/SyosetuParser.ts
+++ src/parser/SyosetuParser.ts
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio';
 export class SyosetuParser extends Parser {
 
     public parseNovelName(html: string):string {
-        let pattern = /class="margin_l10r20">(.*?)<\/a>/gsm;
+        let pattern = /class="margin_r20">(.*?)<\/a>/gsm;
         let match = pattern.exec(html);
         let name;
         if (match && 1 in match) {
@@ -15,11 +15,17 @@ export class SyosetuParser extends Parser {
     }
 
     public parseChapterTitle(html: string):string {
-        let pattern = /<p class="chapter_title">(.*?)<\/p>/gsm;
+        let pattern = /<p class="novel_subtitle">(.*?)<\/p>/gsm;
         let match = pattern.exec(html);
         let title;
         if (match && 1 in match) {
             title = match[1];
+        } else {
+            pattern = /<p class="chapter_title">(.*?)<\/p>/gsm;
+            match = pattern.exec(html);
+            if (match && 1 in match) {
+                title = match[1];
+            }
         }
 
         return title;
